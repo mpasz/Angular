19c5220ebb50f3feec90e0f1710eec4a988060a9
Michal <paszmichal@gmail.com>
2018-08-07 22:40:04 +0200

    AngularFundamentals

---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'courses' ,
+    template: //uzycie dyrektywy poprzedzamy znakiem * UWAGA po dyrektywie ngFor jest pętla analogiczna dp foreach
+        `'<h2>{{ getTitle() }}</h2>'
+         <ul>
+            <li *ngFor="let course of courses">  
+                {{course}}
+            </li>
+         </ul>   
+         `
+})
+
+export class CoursesComponent{
+    title = "List of courses"
+
+    courses = ["course1", "course2", "course3"];
+
+    getTitle(){
+        return this.title
+    }
+
+}
