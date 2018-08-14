d498d20759d793b80c0d0789e2ab28c559a46fda
Michal <paszmichal@gmail.com>
2018-08-14 07:42:36 +0200

    Interpolatrion - use {{ }} for display data

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
+
 import { CoursesSerevice } from './courses.service';
 import {Component} from '@angular/core';
-=======
-import { Component } from '@angular/core';
-import { CoursesService } from './courses.service';
->>>>>>> 31c7bbda2e1040f1e0c349ad1d72fed02eeb20af
+
 
 
 @Component({
@@ -21,27 +18,14 @@ import { CoursesService } from './courses.service';
 
 export class CoursesComponent{
     title = "List of courses";
-<<<<<<< HEAD
     courses;
 
 
     constructor(service: CoursesSerevice) {
-=======
-    courses; 
-
-    constructor(private service: CoursesService) {
-        //let service = new CoursesService();
->>>>>>> 31c7bbda2e1040f1e0c349ad1d72fed02eeb20af
         this.courses = service.getCourses();
     }
 }
 
-
-<<<<<<< HEAD
-
-
     //Logic for calling HTTP service
 
-}
-=======
->>>>>>> 31c7bbda2e1040f1e0c349ad1d72fed02eeb20af
+
--- hello-world/src/app/courses.service.ts
+++ hello-world/src/app/courses.service.ts
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 //component decorator
 export class CoursesSerevice {
     getCourses() {
          return ["Course 1", "Course 2", "Course 3"];
-=======
-export class CoursesService{
-    getCourses(){
-        return ["Course 1","Course 2","Course 3"];
->>>>>>> 31c7bbda2e1040f1e0c349ad1d72fed02eeb20af
     }
 }
