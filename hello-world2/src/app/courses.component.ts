6b621e74eb25a002ea72a1c50d8b9953590d9f39
Michal Pasz <michal.pasz@electropoli.pl>
2018-08-21 10:56:51 +0200

    Displaying Data and Handling Events

--- hello-world2/src/app/courses.component.ts
+++ hello-world2/src/app/courses.component.ts
@@ -4,20 +4,16 @@ import { CoursesService } from './courses.service';
 @Component({ // komponent 
     selector: 'courses',
     template: 
-        `
-        <h2> {{ title }} </h2>
-        <ul>
-            <li *ngFor="let course of courses">
-                {{course}}
-            </li>
-        </ul>
-
+    `
+            <button class ="btn btn-primary">Save</button>
     `
 })
 
 export class CoursesComponent{
  title = "List of courses";
+ imageURL = "http://lorempixel.com/400/200";
  courses;
+ colSpan = 2;
 
  constructor(service: CoursesService) {
      this.courses = service.getCourses();
